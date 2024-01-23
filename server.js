ba1a04cf6084b8d2aa8492384aef4c02f230a6fa
Davide87 <davidetrovato87@gmail.com>
2024-01-23 12:00:06 +0100

    (feat cors)

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static(process.env.STATIC_DIR));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+const cors = require('cors');
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+
+
 app.get("/", (req, res) => {
   const path = resolve(process.env.STATIC_DIR + "/index.html");
   res.sendFile(path);
